2ea4bfe5147b91c8a1c704b2d901614aafd520f0
Mauricio <mauricio.furlan@hotmail.com>
2020-08-05 00:23:26 -0300

    ajustando deploy

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,7 +19,7 @@ interface ObjectToDelete {
 
 const URL = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://pedraoautomoveis.herokuapp.com/categorias';
+  : 'https://pedraoautomoveis.herokuapp.com';
 
 export const getAllCategoriesAsync = async () => {
   const crudeResponse = await fetch(`${URL}/categories`);
