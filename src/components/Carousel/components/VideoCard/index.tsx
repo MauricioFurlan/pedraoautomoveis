77c69fac95d752020b84287a3793130f812f6642
Mauricio Tomesani Furlan <48259868+MauricioFurlan@users.noreply.github.com>
2021-02-02 19:07:10 -0300

    Voltando a mexer no projeto

--- src/components/Carousel/components/VideoCard/index.tsx
+++ src/components/Carousel/components/VideoCard/index.tsx
@@ -43,7 +43,8 @@ const VideoCard = ({
 ];
 
 function importAll(r) {
-  let images: string  = {};
+
+  let images : { [item: string]: string} = {};
   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
   return images;
 }
